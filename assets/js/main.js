a8f9bd099aca06e24b444cb78a2b186b2b742818
Lucia994 <l.summa94@gmail.com>
2025-06-18 16:56:58 +0200

    Finisched the functionality of card print

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,28 @@
 console.log("Hello");
 
-// Seleziono tutti gli elementi dove inserire le immagini manipolando l'elemento in DOM
-const imageEl = document.querySelectorAll(".photo-image");
 
 //fetch dei dati
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
-const imagesList = [];
-fetch(endpoint)//fetch all'API 
+fetch(endpoint) //fetch all'API 
     .then(response => response.json())
-    .then(data => {
-        const images = data; // Prendo gli oggetti dell'array data.response che contiene le 6 immagini.
+    .then(images => {
+        
         console.log(images); //stampo in console
-        imagesList.push(images);
-
-
+        
+        
+        // //Devo inserire ciascuna immagina nel photo images utilizzo il metodo forEach
+        images.forEach((image) => {
+            document.getElementById('cardsList').innerHTML += `
+            <div class="col">
+                <div class="photo-card">
+                    <div class="photo-image" style="background-color: var(--bg-color-first-card);">
+                      <img src="${image.url}" />
+                    </div>
+                    <div class="description">
+                        <div class="description-text">Accusamus Beatae Ad Facilis Cum Similique Qui Sunt</div>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
     })
-console.log(imagesList)
-
-//Devo inserire ciascuna immagina nel photo images utilizzo il metodo forEach
-imagesList.forEach((image, i, array) => {
-    
-    }
-
-)
