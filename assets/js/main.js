85ac056e5f5076843abf7023608ba791ebd5f25b
Lucia994 <l.summa94@gmail.com>
2025-06-18 12:07:00 +0200

    Initial fetch to recover images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 console.log("Hello");
-function fetchData() {
+//fetch dei dati
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/" 
+const imagesList = [];
+fetch(endpoint)//fetch all'API 
+    .then(response => response.json())
+    .then(data => {
+        const images = data; // Prendo gli oggetti dell'array data.response che contiene le 6 immagini.
+        console.log(images); //stampo in console
 
-    fetch("https://lanciweb.github.io/demo/api/pictures/")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-
-}
-
+    })
