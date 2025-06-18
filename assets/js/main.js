53b110d33585f13ca3f985f3c48e168fd98a09b9
Lucia994 <l.summa94@gmail.com>
2025-06-18 11:22:40 +0200

    Test Ajax call

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+function fetchData() {
+
+    fetch("https://lanciweb.github.io/demo/api/pictures/")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+
+}
+
